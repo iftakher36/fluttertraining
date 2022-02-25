c408a89aefe7a11f07159e6738571dc1d4f83b15
iftekhar <si06@gtrbd.net>
2022-02-25 18:23:30 +0600

    responsive dashboard

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'listview_layout.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -16,6 +18,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   String change = "Hello Programmers";
   int counter = 0;
+  double size=0.0;
   double values = 8.0;
   List name = [
     "Farhan",
@@ -33,10 +36,12 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    size=MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(change),
-        leading: IconButton(
+        leading: size>768?null:IconButton(
           icon: const Icon(Icons.menu),
           onPressed: () {
             _Hello;
@@ -54,45 +59,31 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ],
       ),
-      body: Row(
+      body: size>768?Row(
         children: [
           Expanded(
+            flex: 2,
             child: Container(
               color: Colors.red,
-              child: Column(
-                children: const [
-                  SizedBox(height: 20,),
-                  CircleAvatar(
-                    radius: 51,
-                    backgroundColor: Colors.black,
-                    child: CircleAvatar(
-                        radius: 50,
-                        backgroundImage: NetworkImage("https://www.vhv.rs/dpng/d/426-4263064_circle-avatar-png-picture-circle-avatar-image-png.png",scale: 1)
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  TextField(
-                    style: TextStyle(color: Colors.blue),
-                    cursorColor: Colors.blue,
-                    decoration: InputDecoration(
-                      labelText: "UserName",
-                      focusColor: Colors.black,
-                      labelStyle: TextStyle(
-                        color: Color(0xFF0f0f0f),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue)),
-                      prefixIcon: Icon(Icons.supervised_user_circle,
-                          color: Color(0xC3000000)),
+                    const CircleAvatar(
+                      radius: 51,
+                      backgroundColor: Colors.black,
+                      child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage("img/biriyani.jpg")),
                     ),
-                  ),
-                  TextField(
+                    const TextField(
                       style: TextStyle(color: Colors.blue),
                       cursorColor: Colors.blue,
-                      obscureText: true,
                       decoration: InputDecoration(
-                        labelText: "Password",
+                        labelText: "UserName",
                         focusColor: Colors.black,
                         labelStyle: TextStyle(
                           color: Color(0xFF0f0f0f),
@@ -101,98 +92,188 @@ class _LoginScreenState extends State<LoginScreen> {
                             borderSide: BorderSide(color: Colors.black)),
                         focusedBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.blue)),
-                        prefixIcon: Icon(Icons.lock,
+                        prefixIcon: Icon(Icons.supervised_user_circle,
                             color: Color(0xC3000000)),
-                      )),
-                ],
+                      ),
+                    ),
+                    const TextField(
+                        style: TextStyle(color: Colors.blue),
+                        cursorColor: Colors.blue,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          focusColor: Colors.black,
+                          labelStyle: TextStyle(
+                            color: Color(0xFF0f0f0f),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue)),
+                          prefixIcon:
+                              Icon(Icons.lock, color: Color(0xC3000000)),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              side: const BorderSide(
+                                color: Colors.white,
+                                width: 1.0,
+                              ),
+                            )),
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color(0xFF000000)),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "Log In",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
           Expanded(
+              flex: 5,
               child: Container(
                 color: Colors.blue,
-                child: Column(
-            children: const [
-                TextField(
-                  style: TextStyle(color: Colors.blue),
-                  cursorColor: Colors.blue,
-                  decoration: InputDecoration(
-                    labelText: "UserName",
-                    focusColor: Colors.black,
-                    labelStyle: TextStyle(
-                      color: Color(0xFF0f0f0f),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue)),
-                    prefixIcon: Icon(Icons.supervised_user_circle,
-                        color: Color(0xC3000000)),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Card(
+                        color: Colors.white,
+                        child: Container(
+                          height: 100,
+                        ),
+                      ),
+                      ListView.builder(
+                          itemCount: 20,
+                          shrinkWrap: true,
+                          itemBuilder: (rafi,index){
+                            return Card(
+                              child: ListTile(
+                                leading:Image.asset("img/biriyani.jpg"),
+                                title: const Text("hello"),
+                                trailing: const Text("hello"),
+                              ),
+                            );
+                          })
+                    ],
                   ),
                 ),
-                TextField(
-                    style: TextStyle(color: Colors.blue),
-                    cursorColor: Colors.blue,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      focusColor: Colors.black,
-                      labelStyle: TextStyle(
-                        color: Color(0xFF0f0f0f),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue)),
-                      prefixIcon: Icon(Icons.lock,
-                          color: Color(0xC3000000)),
-                    )),
-            ],
-          ),
               )),
-          Expanded(child: Container(
-            color: Colors.orangeAccent,
-            child: Column(
-              children:  [
-                TextField(
-                  style: TextStyle(color: Colors.blue),
-                  cursorColor: Colors.blue,
-                  decoration: InputDecoration(
-                    labelText: "UserName",
-                    focusColor: Colors.black,
-                    labelStyle: TextStyle(
-                      color: Color(0xFF0f0f0f),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue)),
-                    prefixIcon: Icon(Icons.supervised_user_circle,
-                        color: Color(0xC3000000)),
-                  ),
-                ),
-                TextField(
-                    style: TextStyle(color: Colors.blue),
-                    cursorColor: Colors.blue,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      focusColor: Colors.black,
-                      labelStyle: TextStyle(
-                        color: Color(0xFF0f0f0f),
+          Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.orangeAccent,
+                child: Column(
+                  children: [
+                    TextField(
+                      style: TextStyle(color: Colors.blue),
+                      cursorColor: Colors.blue,
+                      decoration: InputDecoration(
+                        labelText: "UserName",
+                        focusColor: Colors.black,
+                        labelStyle: TextStyle(
+                          color: Color(0xFF0f0f0f),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue)),
+                        prefixIcon: Icon(Icons.supervised_user_circle,
+                            color: Color(0xC3000000)),
                       ),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue)),
-                      prefixIcon: Icon(Icons.lock,
-                          color: Color(0xC3000000)),
-                    )),
-              ],
-            ),
-          )),
+                    ),
+                    TextField(
+                        style: TextStyle(color: Colors.blue),
+                        cursorColor: Colors.blue,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          focusColor: Colors.black,
+                          labelStyle: TextStyle(
+                            color: Color(0xFF0f0f0f),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue)),
+                          prefixIcon:
+                              Icon(Icons.lock, color: Color(0xC3000000)),
+                        )),
+                  ],
+                ),
+              )),
         ],
-      ),
+      ):Row(
+          children: [
+            Expanded(
+                flex: 5,
+                child: Container(
+                  color: Colors.blue,
+                  child: Column(
+                    children: [
+                      Card(
+                        color: Colors.white,
+                        child: Container(
+                          height: 100,
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.orangeAccent,
+                  child: Column(
+                    children: [
+                      TextField(
+                        style: TextStyle(color: Colors.blue),
+                        cursorColor: Colors.blue,
+                        decoration: InputDecoration(
+                          labelText: "UserName",
+                          focusColor: Colors.black,
+                          labelStyle: TextStyle(
+                            color: Color(0xFF0f0f0f),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue)),
+                          prefixIcon: Icon(Icons.supervised_user_circle,
+                              color: Color(0xC3000000)),
+                        ),
+                      ),
+                      TextField(
+                          style: TextStyle(color: Colors.blue),
+                          cursorColor: Colors.blue,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            focusColor: Colors.black,
+                            labelStyle: TextStyle(
+                              color: Color(0xFF0f0f0f),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black)),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blue)),
+                            prefixIcon:
+                            Icon(Icons.lock, color: Color(0xC3000000)),
+                          )),
+                    ],
+                  ),
+                )),
+          ],
+        )
     );
   }
 }
